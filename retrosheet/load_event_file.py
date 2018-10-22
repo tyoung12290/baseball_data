a40fc8b111706818f84f94e89c90c0056ae5f46c
Tyler Young <youngty@tylers-mbp.home>
2018-10-22 17:14:47 -0500

    jupyter

--- retrosheet/load_event_file.py
+++ retrosheet/load_event_file.py
@@ -2,8 +2,10 @@
 #conn = psycopg2.connect("host=localhost dbname=lahman2017 user=lahman")
 
 import subprocess
+import pandas
 path = '/Users/youngty/Projects/baseball_data/retrosheet/2017/2017ANA.csv'
 with open(path, "a") as file:
     file.flush()
-    subprocess.call(['cwevent','-y', '2017', 'a' , '2017ANA.EVA'],cwd='2017', stdout=file)
-#df = pandas.read_csv('/Projects/baseball_data/retrosheet/2017/2017ANA.csv')
+    subprocess.call(['cwevent','-y', '2017', '-a' , '2017ANA.EVA'],cwd='2017', stdout=file)
+df = pandas.read_csv('/Users/youngty/Projects/baseball_data/retrosheet/2017/2017ANA.csv')
+print(df)
